cb85c2941debd17a8af54138065e1806ad75ee72
takahashi <7567050+MasaYan24@users.noreply.github.com>
2023-04-23 05:56:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Neovim setting
-git clone https://github.com/MasaYan24/vim.git $HOME/.vim
-sh $HOME/.vim/install.sh
-
 # Prompto setting
 mkdir -p $HOME/.config && echo "command_timeout = 2000" > $HOME/.config/starship.toml
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -15,6 +11,10 @@ installer=$(mktemp -d)/miniconda_install.sh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
   -O $installer
 sh $installer -b -p $HOME/.miniconda
+
+# Neovim setting
+git clone https://github.com/MasaYan24/vim.git $HOME/.vim
+sh $HOME/.vim/install.sh
 
 # git setting
 git config --global core.editor vi
